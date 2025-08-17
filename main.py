93cb799407e4c4478b212fd997de588d6258a642
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-17 20:08:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ def home():
 def health():
     return {"status": "ok"}
 
-# Jangan jalankan uvicorn kalau di production
+# Hanya jalankan server jika di lokal
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
