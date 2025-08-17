47aaf9a73eaeae04e868621ce15298e7a0e34999
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-17 13:04:51 +0700

    Create Typewriter.jsx

---
+++ frontend/src/components/Typewriter.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+export default function Typewriter({ text, speed = 30 }) {
+  const [displayedText, setDisplayedText] = useState('');
+
+  useEffect(() => {
+    setDisplayedText('');
+    let i = 0;
+    const timer = setInterval(() => {
+      if (i < text.length) {
+        setDisplayedText(text.slice(0, i + 1));
+        i++;
+      } else {
+        clearInterval(timer);
+      }
+    }, speed);
+
+    return () => clearInterval(timer);
+  }, [text, speed]);
+
+  return <span>{displayedText}</span>;
+}
