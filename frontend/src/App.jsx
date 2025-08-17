114542d94e806b71e6a27abe7ca5c11e7cbe5e02
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-17 13:02:55 +0700

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import ChatInterface from './components/ChatInterface';
+
+function App() {
+  return (
+    <div className="font-sans">
+      <ChatInterface />
+    </div>
+  );
+}
+
+export default App;
